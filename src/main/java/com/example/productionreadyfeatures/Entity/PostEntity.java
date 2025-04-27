87389950ce4bb92c22ad4104d8e4796ec1d2b86e
mdswaley <mdswaley075@gmail.com>
2025-04-27 10:09:30 +0530

    update

--- src/main/java/com/example/productionreadyfeatures/Entity/PostEntity.java
+++ src/main/java/com/example/productionreadyfeatures/Entity/PostEntity.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "postData")
-@Audited //this annotation means inside this class all the field are audited now(we can see who ever change and which time change)
+@Audited //this annotation means inside this class all the field are audited now(we can see who ever change any field and when it changes)
 public class PostEntity extends AuditingEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,15 +28,19 @@ public class PostEntity extends AuditingEntity {
 //    we can paste our auditing data in parent class so that other entity can easily extend it and use it.
 
     @PrePersist
-    void beforeSave(){
+    void beforeSave() {
+        System.out.println("Saving a new Post: " + title);
     }
 
     @PreUpdate
-    void beforeUpdate(){
+    void beforeUpdate() {
+        System.out.println("Updating the Post: " + title);
+    }
 
-    }
     @PreRemove
-    void beforeRemove(){
+    void beforeRemove() {
+        System.out.println("Deleting the Post: " + title);
     }
+
 
 }
